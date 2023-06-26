7479b84c36cd8f8be8ece86680aae765cc905f46
Barry Hanna <barryahanna@gmail.com>
2023-06-26 12:19:17 +0100

    Exercise 4.11

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -52,6 +52,20 @@ describe('blog tests', () => {
 			id: result.body.id,
 		});
 	});
+
+	test('missing likes will default to 0', async () => {
+		const newBlog = {
+			title: 'New Blog - Test Missing Likes Results in likes being 0',
+			author: 'Test Author',
+			url: 'http://newblog.com',
+		};
+		const result = await api
+			.post('/api/blogs')
+			.send(newBlog)
+			.expect(201);
+
+		expect(result.body.likes).toBe(0);
+	});
 });
 
 afterAll(async () => {
