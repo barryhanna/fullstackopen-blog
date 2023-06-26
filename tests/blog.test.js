0febbec4221d977dfb63c082059eed351105f3b7
Barry Hanna <barryahanna@gmail.com>
2023-06-26 10:29:37 +0100

    Add tests and refactor route handlers to use async/await

---
+++ tests/blog.test.js
@@ -0,0 +1,33 @@
+const supertest = require('supertest');
+const helper = require('../utils/test_helper');
+const mongoose = require('mongoose');
+const app = require('../app');
+const api = supertest(app);
+
+mongoose.set('bufferTimeoutMS', 30000);
+
+const Blog = require('../models/blog');
+
+beforeEach(async () => {
+	await Blog.deleteMany({});
+
+	for (let blog of helper.initialBlogs) {
+		let blogObject = new Blog(blog);
+		await blogObject.save();
+	}
+}, 100000);
+
+describe('blog tests', () => {
+	test('correct amount of blog posts return in JSON format', async () => {
+		const initialNumberOfBlogs = helper.initialBlogs.length;
+		const result = await api
+			.get('/api/blogs')
+			.expect(200)
+			.expect('Content-Type', /application\/json/);
+		expect(result.body.length).toEqual(initialNumberOfBlogs);
+	}, 100000);
+});
+
+afterAll(async () => {
+	await mongoose.connection.close();
+});
